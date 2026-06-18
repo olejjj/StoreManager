03889acf77728789d11ab8926a6bcdb5282973dd
Krzysztof_laptop <olej1155@gmail.com>
2026-06-18 10:57:39 +0200

    feat: add pagination and search filtering to products

--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Product } from './product.entity';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Repository, ILike } from 'typeorm';
 
 @Injectable()
 export class ProductService {
@@ -12,11 +12,29 @@ export class ProductService {
     private readonly productRepository: Repository<Product>,
   ) {}
 
-  // Zwraca wszystkie produkty, które nie sa usunięte
-  async findAll() {
-    return await this.productRepository.find({
-      where: { isDeleted: false },
+  // Zwraca wszystkie produkty, które nie sa usunięte, przyjmuje stronę, limit i szukane słowo
+  async findAll(page: number, limit: number, search?: string) {
+    const skip = (page - 1) * limit; // Obliczamy, ile rekordów pominąć (Offset)
+
+    const [items, total] = await this.productRepository.findAndCount({
+      where: { 
+        isDeleted: false,
+        ...(search ? { title: ILike(`%${search}%`) } : {})
+      },
+      take: limit, 
+      skip: skip,  
     });
+
+    // zwraca obiekt
+    return {
+      data: items,
+      meta: {
+        totalItems: total,
+        currentPage: page,
+        itemsPerPage: limit,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   }
 
   // tworzy nowy produkt
